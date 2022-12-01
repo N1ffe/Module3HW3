107db6e70517f8a58b668e34556544edffc9bc73
Niffe <petrovnik01@ukr.net>
2022-12-01 18:04:18 +0200

    Task

---
+++ Module3HW3/Module3HW3/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Module3HW3
+{
+    public class Class2
+    {
+        public delegate int Delegate1(int value1, int value2);
+        public delegate bool Delegate2(int value);
+        public int Value { get; private set; }
+        public Delegate2 Calc(Delegate1 pow, int value1, int value2)
+        {
+            Value = pow(value1, value2);
+            return Result;
+        }
+        public bool Result(int value)
+        {
+            return (Value % value) == 0;
+        }
+    }
+}
--- Module3HW3/Module3HW3/Program.cs
+++ Module3HW3/Module3HW3/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Module3HW3
+﻿using System.Reflection;
+
+namespace Module3HW3
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Class1 c1 = new Class1();
+            Class2 c2 = new Class2();
+            Class1.Delegate showDelegate = Show;
+            showDelegate.Invoke(c2.Calc(c1.Pow, 5, 8).Invoke(4));
+        }
+        public static void Show(bool value)
+        {
+            Console.WriteLine(value);
         }
     }
 }
